9237676492b90b56c1864e0529ef15cd04ae4767
Thiago Amaral <thiago-amaral@msn.com>
2019-06-13 13:01:24 -0300

    Componente Botão #01

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from "./components/Button"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Calculadora de teste</Text>
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttons}>
+          <Button label='AC'/>
+          <Button label='/'/>
+          <Button label='7' />
+          <Button label='8'  />
+          <Button label='9'/>
+          <Button label='*'/>
+          <Button label='4' />
+          <Button label='5' />
+          <Button label='6' />
+          <Button label='-'  />
+          <Button label='1'/>
+          <Button label='2'  />
+          <Button label='3'  />
+          <Button label='+'  />
+          <Button label='0' />
+          <Button label='.'  />
+          <Button label='='  />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  }
 });
